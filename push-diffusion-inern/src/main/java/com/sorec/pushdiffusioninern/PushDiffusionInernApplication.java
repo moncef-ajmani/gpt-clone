41d142e884ba5dc022487648d6eb6dd51c219b23
moncef-ajmani <moncef.ajmani@gmail.com>
2024-03-08 10:11:32 +0100

    initial project

---
+++ push-diffusion-inern/src/main/java/com/sorec/pushdiffusioninern/PushDiffusionInernApplication.java
@@ -0,0 +1,13 @@
+package com.sorec.pushdiffusioninern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushDiffusionInernApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushDiffusionInernApplication.class, args);
+    }
+
+}
---
+++ push-diffusion-inern/src/test/java/com/sorec/pushdiffusioninern/PushDiffusionInernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorec.pushdiffusioninern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushDiffusionInernApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
